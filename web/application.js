95e64f8d21aee7c34e4644673a6b6f36504b16d5
Matias Insaurralde <matias@insaurral.de>
2016-03-14 19:23:53 -0300

    Removing web stuff

--- web/application.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  var es = new EventSource( 'http://127.0.0.1:5000/' )
-  console.log( 'es' )
-  es.onmessage = function (event) {
-    console.log('message', event, event.data )
-  }
-
-}
