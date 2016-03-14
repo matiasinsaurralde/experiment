ca7416d20bd599382870f47aef574f0a9737dc1b
Matias Insaurralde <matias@insaurral.de>
2016-03-14 06:39:22 -0300

    Updating Go implementation

--- server/golang/main.go
+++ server/golang/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import(	"fmt"
+	"io/ioutil"
+	"net/http" )
+
+var Endpoints = []string{
+  "http://127.0.0.1:2000/",
+  "http://127.0.0.1:2001/",
+  "http://127.0.0.1:2002/",
+  "http://127.0.0.1:2003/",
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Println( "handler" )
+
+  flusher, ok := w.(http.Flusher)
+
+  if !ok {
+    fmt.Println( "can't initialize flusher")
+    return
+  }
+
+  endpointChan := make(chan []byte)
+
+  for _, endpoint := range Endpoints {
+    go func( endpoint string ) {
+      fmt.Println( "Sending req to endpoint " + endpoint )
+      resp, _ := http.Get( endpoint )
+      body, _ := ioutil.ReadAll( resp.Body )
+      resp.Body.Close()
+      fmt.Println( body )
+      endpointChan <- body
+    }(endpoint)
+  }
+
+  notify := w.(http.CloseNotifier).CloseNotify()
+
+  go func() {
+    <-notify
+    fmt.Println( "closed" )
+  }()
+
+  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+  w.WriteHeader(http.StatusOK)
+
+
+  for {
+    fmt.Println( "write?" )
+    w.Write( <-endpointChan )
+    w.Write( []byte("\n") )
+
+    flusher.Flush()
+  }
+
+}
 
 func main() {
-  fmt.Println( "hello" )
+    fmt.Println( "init" )
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":5000", nil)
 }
